0f6fa8498e708665329eb6683f9850f324659a0a
Squeezr <samxiezx1115@gmail.com>
2023-09-22 14:27:03 -0400

    image rendering issue fixed and li element fixed
    haha

--- src/pages/Quiz/Quiz.tsx
+++ src/pages/Quiz/Quiz.tsx
@@ -1,6 +1,6 @@
 import './Quiz.css'
 import { QuizInfo } from '../../models/quizModel';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 interface QuizProps {
     quizList: QuizInfo[];
@@ -9,25 +9,41 @@ interface QuizProps {
 const Quiz = ({ quizList }: QuizProps): JSX.Element => {
 
     const [questionIndex, setQuestionIndex] = useState(1)
+    const [imgloaded, setImageLoaded] = useState(false)
     const [timer, setTimer] = useState(10)
 
     let options = quizList[questionIndex - 1].options
+
+    useEffect(() => {
+
+        const loadImage = () => {
+            const img = new Image()
+            img.src = quizList[questionIndex - 1].imageUrl
+            img.onload = () => {
+                setImageLoaded(true)
+            }
+        }
+
+        loadImage()
+    }, [questionIndex, quizList])
 
 
     return (
         <div className="Quiz">
             <div className="App-header">
                 <div className="quiz-body">
-                    <div className="question-frame">
-                        <h1>Quiz 0/{questionIndex}</h1>
-                        <img src={quizList[questionIndex - 1].imageUrl} alt={"question" + questionIndex.toString()}></img>
-                        <p>{quizList[questionIndex - 1].question}</p>
-                        <div className="option-frame">
-                            {options.map((item) => {
-                                return <p>{item}</p>
-                            })}
+                    {imgloaded ? (
+                        <div className="question-frame">
+                            <h1>Quiz {questionIndex}/{quizList.length}</h1>
+                            <img src={quizList[questionIndex - 1].imageUrl} alt={"question" + questionIndex.toString()}></img>
+                            <p>{quizList[questionIndex - 1].question}</p>
+                            <ul className="option-frame">
+                                {options.map((item, index) => {
+                                    return <li className={"selection-" + index} key={"selection-" + index}>{item}</li>
+                                })}
+                            </ul>
                         </div>
-                    </div>
+                    ) : (<div></div>)}
                 </div>
             </div>
         </div >
